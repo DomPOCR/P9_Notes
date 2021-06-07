8737f25a37497ad16ee2166865a2211ff5afc720
DomPOCR <dominiquep.technocenter@gmail.com>
2021-06-07 08:49:26 +0200

    Initial commit

---
+++ src/test/java/com/mediscreen/p9_notes/P9NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.p9_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P9NotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
